9efd2d4c7946d7140ef46516d0562acd25f47f2c
velmyk <velmyk@gmail.com>
2016-09-05 18:04:40 +0300

    feat(data) download players records

---
+++ dataFetching.js
@@ -0,0 +1,43 @@
+const
+	fs = require('fs'),
+	Client = require('node-rest-client').Client,
+	client = new Client();
+
+let allPlayers = [];
+
+[...Array(60).keys()].forEach(getTeamPlayers);
+
+function getTeamPlayers(teamId) {
+	const args = {
+		headers: {
+			'X-Auth-Token': '506fded9507f4d35a31fae98edad9e02'
+		}
+	};
+
+	client.get(`http://api.football-data.org/v1/teams/${teamId}/players`, args, data => {
+		if (data._links && data.players.length) {
+			allPlayers = [
+				...allPlayers,
+				...applyTeamToPlayer(teamId, data.players)
+			];
+		} else {
+			console.log(`no data for team #${teamId}`);
+		}		
+	});
+}
+
+setTimeout(() => {
+	console.log(allPlayers.length);
+	writeDataToFile(allPlayers);
+}, 5000);
+
+function applyTeamToPlayer(teamId, players) {
+	return players.map(player => Object.assign(player, { teamId }));
+}
+
+function writeDataToFile(data) {
+	const players = JSON.stringify(data);
+	fs.writeFile('./players.json', players, err => {
+		if (err) console.error(err);
+	});
+}
